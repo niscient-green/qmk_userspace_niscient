ffca3a0104644a150190350746c5905cc639e58b
Nick Hofmeister <5778927+niscient-green@users.noreply.github.com>
2024-03-17 15:02:05 -0400

    Add rev3_0.6.0 files

--- keyboards/splitkb/kyria/rev3/keymaps/niscient/config.h
+++ keyboards/splitkb/kyria/rev3/keymaps/niscient/config.h
@@ -16,22 +16,31 @@
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    ifndef RGBLIGHT_LIMIT_VAL
-#    	define RGBLIGHT_LIMIT_VAL 150
-#    endif
+#ifdef RGB_MATRIX_ENABLE
+
+// The number of LEDs connected
+#    define RGB_MATRIX_LED_COUNT 62
+#    define DRIVER_LED_TOTAL 62
+
+// show this is a splitkb
+#    define RGB_MATRIX_SPLIT \
+        { 31, 31 }
+#    define SPLIT_TRANSPORT_MIRROR
+
+// set up matrix params
+#    define RGB_MATRIX_KEYPRESSES                                   // reacts to keypresses
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true                     // turn off effects when suspended
+#    define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#    define RGB_MATRIX_LED_FLUSH_LIMIT 16                           // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    undef RGB_MATRIX_MAXIMUM_BRIGHTNESS                            // is preset in info_config.h:49
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150                       // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+
 #endif
 
+#define LED_CAPS_LOCK_PIN 24
+#define LED_PIN_ON_STATE 0
--- keyboards/splitkb/kyria/rev3/keymaps/niscient/rules.mk
+++ keyboards/splitkb/kyria/rev3/keymaps/niscient/rules.mk
@@ -1,4 +1,13 @@
-OLED_ENABLE = yes
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+OLED_ENABLE = yes					# Enable OLED displays
+ENCODER_ENABLE = yes	       		# Enables the use of one or more encoders
+LEADER_ENABLE = no         			# Enable the Leader Key feature
+MOUSEKEY_ENABLE = no				# Enable the mouse key feature
+KEY_OVERRIDE_ENABLE = yes  			# Enable key overrides for things like Shift + Bksp = Del
+CONVERT_TO=liatris					# Output uf2 and liatris 2040 format
+
+# lighting
+WS2812_DRIVER = vendor 				# Use the RP2040's PIO interface
+RGBLIGHT_ENABLE = no 				# Enables QMK's RGB code
+RGB_MATRIX_ENABLE = yes				# enable rgb matrix lighting
+RGB_MATRIX_DRIVER = ws2812			# matrix lighting uses ws2812 driver (note: driver must be lower case wtf)
+SPLIT_KEYBOARD = yes				# notify qmk of split
