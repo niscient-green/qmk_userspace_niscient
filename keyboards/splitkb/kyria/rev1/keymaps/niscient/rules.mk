5ad535e2758afde422d275f9c27a9f2bc75b5c17
Nick Hofmeister <5778927+niscient-green@users.noreply.github.com>
2024-03-17 14:51:16 -0400

    Add version 1.0.0 of rev1 files

---
+++ keyboards/splitkb/kyria/rev1/keymaps/niscient/rules.mk
@@ -0,0 +1,5 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+KEY_OVERRIDE_ENABLE = yes  # Enable key overrides for things like Shift + Bksp = Del
